2ab2f8b5fe78388c2cfd179bd77d789bf564e133
Sergei Chestakov <schestakov@ucdavis.edu>
2018-04-25 22:43:49 -0700

    k Empty Slots

---
+++ kEmptySlots.py
@@ -0,0 +1,26 @@
+# There is a garden with N slots. In each slot, there is a flower. The N flowers will bloom one by one in N days.
+# In each day, there will be exactly one flower blooming and it will be in the status of blooming since then.
+# Given an array flowers consists of number from 1 to N. Each number in the array represents the place where the flower will open in that day.
+# For example, flowers[i] = x means that the unique flower that blooms at day i will be at position x, where i and x will be in the range from 1 to N.
+# Given an integer k, you need to output in which day there exists two flowers in the status of blooming, and there are k flowers between them that are not blooming
+# If there isn’t such day, output -1.
+
+class Solution:
+    def kEmptySlots(self, flowers, k):
+        length = len(flowers) - k - 1
+        for index in range(length):
+            d1 = flowers[index]
+            d2 = flowers[index + k + 1]
+            found = True
+
+            #All days in between must be > d1 & d2
+            for day in range(1, k + 1):
+                inBetween = flowers[index + day]
+                if inBetween < d1 or inBetween < d2: #Flower has bloomed so break
+                    found = False
+                    break
+
+            if found:
+                return d1 if d1 > d2 else d2 #largest of the two
+
+        return -1
