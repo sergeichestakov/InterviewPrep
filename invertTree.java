2f3a30430ec3d18389dcd7831ce9c77505138c67
Sergei Chestakov <schestakov@ucdavis.edu>
2018-05-08 18:29:12 -0700

    Simplify Invert Tree

--- invertTree.java
+++ invertTree.java
@@ -13,11 +13,8 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null){ return root; }
         TreeNode left = root.left;
-        TreeNode right = root.right;
-        if(left != null || right != null){
-            root.left = invertTree(right);
-            root.right = invertTree(left);
-        }
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
         return root;
     }
 }
