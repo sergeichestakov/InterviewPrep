a25e0a469d33474bf9e77b82c0cb2a5288608e32
Sergei Chestakov <schestakov@ucdavis.edu>
2018-08-24 14:41:14 -0700

    Max Profit

---
+++ maxProfit.py
@@ -0,0 +1,15 @@
+# Say you have an array for which the ith element is the price of a given stock on day i.
+# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+# Note that you cannot sell a stock before you buy one.
+
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxProfit, minPrice = 0, float('inf')
+        for price in prices:
+            maxProfit = max(maxProfit, price - minPrice)
+            minPrice = min(minPrice, price)
+        return maxProfit
